0bb11038cbd8b80adb5c1a4f7624a631a97bb5eb
Daramola Ayobami <ayox2012@gmail.com>
2021-05-11 14:38:18 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,34 @@
+## SIMPLE EXPRESS SERVER 
+
+## Installation and Setup Instructions
+
+Clone down this repository. You will need `node` and `npm` installed globally on your machine.  
+
+Installation:
+
+`npm install`      
+
+To Start Server:
+
+`npm run dev`  
+
+To Visit App:
+
+`localhost:6005/`  
+
+## ROUTES:
+
+CREATE:
+POST http://localhost:6005/person;
+
+
+Get Data Created:
+GET http://localhost:6005/people;
+
+UPDATE:
+ 
+PUT http://localhost:6005/person/:id
+ 
+DELETE:
+ 
+DELETE http://localhost:6005/person/:id
---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const Mongoose = require("mongoose");
+const { PORT } = process.env;
+
+var app = express();
+
+Mongoose.connect("mongodb://localhost:27017/task5");
+
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.json({ message:"succes" }));
+
+const PeopleInfo = Mongoose.model("person", {
+    name: String,
+    email: String,
+    country: String
+});
+
+app.post("/person", async (request, response) => {
+    try {
+        var person = new PeopleInfo(request.body);
+        var result = await person.save();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.get("/people", async (request, response) => {
+    try {
+        var result = await PeopleInfo.find().exec();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.get("/person/:id", async (request, response) => {
+    try {
+        var person = await PeopleInfo.findById(request.params.id).exec();
+        response.send(person);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.put("/person/:id", async (request, response) => {
+    try {
+        var person = await PeopleInfo.findById(request.params.id).exec();
+        person.set(request.body);
+        var result = await person.save();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.delete("/person/:id", async (request, response) => {
+    try {
+        var result = await PeopleInfo.deleteOne({ _id: request.params.id }).exec();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+
+app.listen(6005, () => {
+    console.log(`server is listening on port 6005...`)
+});
